e6641d98f50f48b9de41eeb28d7fe3457d85bf76
TamaMara18 <39649956+TamaMara18@users.noreply.github.com>
2018-10-10 11:15:56 +0200

    Menu and board (0.1)

---
+++ tictactoemenu.py
@@ -0,0 +1,38 @@
+board = [0, 1, 2,
+         3, 4, 5,
+         6, 7, 8]
+
+def show():
+    print(board[0], '|', board[1], '|', board[2])
+    print('---------')
+    print(board[3], '|', board[4], '|', board[5])
+    print('---------')
+    print(board[6], '|', board[7], '|', board[8])
+
+
+def menu():
+    print("\n************MAIN MENU**************")
+    print(" ")
+    print("1: Game with mate\n2: Game with computer\n3: Computer vs computer\n4: Quit""")
+    
+    menu_choice = False
+    menu_choice = str(input("The number of your choice: "))
+   
+    while True:
+        if menu_choice == "1":
+            print("with_mate()")
+            menu()
+        elif menu_choice == "2":
+            print("with_com()")
+            menu()
+        elif menu_choice == "3":
+            print("com_vs_com()")
+            menu()
+        elif menu_choice == "4":
+            exit() 
+        else:
+            print("Invalid input! Please enter: 1 / 2 / 3 / 4")
+            menu() 
+
+
+print(menu())
